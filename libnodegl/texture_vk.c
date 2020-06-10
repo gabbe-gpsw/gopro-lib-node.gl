7ec524ab00d03563185e49486cc5e64c69954999
Matthieu Bouron <matthieu.bouron@gmail.com>
2020-06-10 17:51:11 +0200

    texture_vk: fix aspect for x8_d24

--- libnodegl/texture_vk.c
+++ libnodegl/texture_vk.c
@@ -68,6 +68,7 @@ static int is_depth_format(VkFormat format)
     case VK_FORMAT_D24_UNORM_S8_UINT:
     case VK_FORMAT_D32_SFLOAT:
     case VK_FORMAT_D32_SFLOAT_S8_UINT:
+    case VK_FORMAT_X8_D24_UNORM_PACK32:
         return 1;
     default:
         return 0;
@@ -79,6 +80,7 @@ static VkImageAspectFlags get_vk_image_aspect_flags(VkFormat format)
     switch (format) {
     case VK_FORMAT_D16_UNORM:
     case VK_FORMAT_D32_SFLOAT:
+    case VK_FORMAT_X8_D24_UNORM_PACK32:
         return VK_IMAGE_ASPECT_DEPTH_BIT;
     case VK_FORMAT_D16_UNORM_S8_UINT:
     case VK_FORMAT_D24_UNORM_S8_UINT:
